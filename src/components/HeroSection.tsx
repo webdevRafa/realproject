4c9a398f341346990a0eaf35a70aef8e1236e671
Rafael Castro <ralphc1992@live.com>
2024-09-12 10:32:53 -0500

    dumb fixes

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,7 +5,7 @@ export const HeroSection: React.FC = () => {
   return (
     <>
       <div className="w-full shadow-sm bg-black">
-        <div className="mx-auto w-[90%] max-w-[800px] pt-20 pb-40">
+        <div className="mx-auto w-[90%] max-w-[600px] pt-20 pb-40">
           <div className="flex justify-between md:justify-around">
             <div className="pt-5 animate-shadow rounded-full overflow-hidden flex-1">
               <img
@@ -16,21 +16,23 @@ export const HeroSection: React.FC = () => {
             </div>
 
             <div className="grow flex items-center justify-center flex-1">
-              <div className="mx-[20px] md:mx-[50px]">
-                <h1 className="text-white font-bold text-xl">
-                  <TypingEffect
-                    text={["Welcome"]}
-                    startDelay={1500}
-                    typeSpeed={80}
-                  />
-                </h1>
-                <p className="text-gray-light">
-                  <TypingEffect
-                    text={["I'm just a front-end developer"]}
-                    startDelay={2000}
-                    typeSpeed={100}
-                  />
-                </p>
+              <div className="flex items-center justify-center pl-10">
+                <div>
+                  <h1 className="text-white text-xl">
+                    <TypingEffect
+                      text={["Welcome"]}
+                      startDelay={1500}
+                      typeSpeed={80}
+                    />
+                  </h1>
+                  <p className="text-gray-light">
+                    <TypingEffect
+                      text={["I'm just a front-end developer"]}
+                      startDelay={2000}
+                      typeSpeed={100}
+                    />
+                  </p>
+                </div>
               </div>
             </div>
           </div>
