588a00710986a9b2b70448b4f19d60cfaa47b8c2
Rafael Castro <ralphc1992@live.com>
2024-09-14 11:12:54 -0500

    real push animations for sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { About } from "./components/About";
 import { HeroSection } from "./components/HeroSection";
 import { Navbar } from "./components/Navbar";
 import { Sections } from "./components/Sections";
